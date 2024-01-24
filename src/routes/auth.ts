5bb16a0213b2edbafe106b651ed1cb0719142380
Jan Maslov <janmaslov@outlook.de>
2024-01-24 22:56:17 +0100

    Remove useless routing files and database from repo

--- src/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-/* import Elysia from "elysia";
-import { renderIndexPage } from "../handlers";
-import { app } from "..";
-
-const authRoutes = (app: Elysia) => app
-
-
-export default authRoutes; */
